77782b70caa00234cb0800df23dd49f755db503a
Dudarik <Dudarik@yandex.ru>
2024-07-20 08:07:01 +0500

    feat: add substate

---
+++ src/components/Substrate.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+interface IProps {
+  closeButton?: boolean;
+}
+const props = withDefaults(defineProps<IProps>(), {
+  closeButton: false,
+});
+const emit = defineEmits<{
+  (e: 'close'): void;
+}>();
+
+const onClickClose = () => {
+  emit('close');
+};
+</script>
+
+<template>
+  <div class="substrate">
+    <button
+      class="close_btn"
+      v-if="props.closeButton"
+      @click.stop="onClickClose"
+    >
+      <span class="line left_line"></span>
+      <span class="line right_line"></span>
+    </button>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+$border-radius: 0.5rem;
+$border-color: #4d4d4d;
+$bg-color: #262626;
+
+.substrate {
+  position: relative;
+
+  padding: 1rem;
+
+  background-color: #262626dd;
+
+  border-radius: $border-radius;
+
+  border: 1px solid $border-color;
+
+  backdrop-filter: blur(5px);
+
+  .close_btn {
+    $btn-size: 2.5rem;
+
+    cursor: pointer;
+
+    position: absolute;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background-color: $bg-color;
+
+    border: none;
+    border-radius: $border-radius;
+
+    top: 0.5rem;
+    right: 0.5rem;
+
+    width: $btn-size;
+    height: $btn-size;
+    transition: transform 0.3s;
+    &:hover {
+      transform: rotate(90deg);
+    }
+
+    &:hover > .line {
+      background-color: #9b59b6;
+    }
+
+    .line {
+      position: absolute;
+      width: 2.5rem;
+      height: 0.3rem;
+      background-color: white;
+      border-radius: 0.2rem;
+      transition: background-color 0.3s;
+
+      &.left_line {
+        transform: rotate(45deg);
+      }
+      &.right_line {
+        transform: rotate(-45deg);
+      }
+    }
+  }
+}
+</style>

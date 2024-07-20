e1243863c3c673d997a1f6439f368b9f035d9454
Dudarik <Dudarik@yandex.ru>
2024-07-20 07:25:35 +0500

    feat: add sceleton

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Skeleton from './components/Skeleton.vue';
+</script>
 
-<template>App</template>
+<template>
+  <div class="container">
+    <Skeleton width="18rem" height="2rem" borderRadius="1rem" />
+    <Skeleton width="14rem" height="1rem" borderRadius=".25rem" />
+    <Skeleton width="14rem" height="1rem" borderRadius=".1rem" />
+    <Skeleton width="14rem" height="1rem" />
+  </div>
+</template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1rem;
+}
+</style>
---
+++ src/components/Skeleton.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+interface IProps {
+  width: string;
+  height: string;
+  borderRadius?: string;
+}
+
+const DEFAULT_BORDER_RADIUS = '0.5rem';
+
+const { width, height, borderRadius } = withDefaults(defineProps<IProps>(), {
+  borderRadius: DEFAULT_BORDER_RADIUS,
+});
+</script>
+
+<template>
+  <div class="skeleton">
+    <div
+      class="skeleton_wrapper"
+      :style="{
+        width,
+        height,
+        borderRadius,
+      }"
+    >
+      <div
+        class="skeleton_gradient"
+        :style="{
+          width,
+          height,
+        }"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.skeleton {
+  display: flex;
+  .skeleton_gradient {
+    position: absolute;
+    background-image: linear-gradient(
+      90deg,
+      #3c3c3c 0%,
+      #444444 51%,
+      #3c3c3c 100%
+    );
+    width: 10rem;
+
+    animation: shine-lines 2s infinite linear;
+  }
+  .skeleton_wrapper {
+    position: relative;
+    background-color: #3c3c3c;
+    overflow: hidden;
+  }
+}
+
+@keyframes shine-lines {
+  0% {
+    left: -100%;
+  }
+
+  40% {
+    left: 100%;
+  }
+  100% {
+    left: 100%;
+  }
+}
+</style>
